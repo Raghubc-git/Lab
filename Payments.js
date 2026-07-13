87db805a9293c4918fdcb699f0ad998d7fe946c4
jyothi <jyothi.dk@consolewaves.com>
2026-07-13 17:14:43 -0500

    Lab-321 - Added js and json files

---
+++ Payments.js
@@ -0,0 +1,13 @@
+let i = 1;
+let j = 3;
+let k = k++;
+
+compare(a, b)
+{
+    if (a > b) {
+        console.log("a is greater than b");
+    } else {
+        console.log("b is greater than a");
+    }
+}
+compare(i,j);
